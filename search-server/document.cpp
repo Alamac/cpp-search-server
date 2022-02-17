888b67e172f6531db86b61862b916784f9ec14b8
Nikita Ignatov <alamacus@gmail.com>
2022-02-17 18:10:51 +0300

    Fix issues

--- search-server/document.cpp
+++ search-server/document.cpp
@@ -5,6 +5,9 @@
 
 using namespace std::string_literals;
 
+Document::Document() : id(0), relevance(0.0), rating(0) {};
+Document::Document(int id, double relevance, int rating) : id(id), relevance(relevance), rating(rating) {};
+
 std::ostream& operator<<(std::ostream& os, Document doc) {
     os << "{ document_id = "s << doc.id << ", relevance = "s << doc.relevance << ", rating = "s << doc.rating << " }"s;
     return os;
--- search-server/document.h
+++ search-server/document.h
@@ -1,8 +1,8 @@
 #pragma once
 
 struct Document {
-    Document() : id(0), relevance(0.0), rating(0) {};
-    Document(const int& id, const double& relevance, const int& rating) : id(id), relevance(relevance), rating(rating) {};
+    Document();
+    Document(int id, double relevance, int rating);
     int id;
     double relevance;
     int rating;
